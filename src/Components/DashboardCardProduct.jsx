2b65ef682e51545b699b421b4b92041c6732903b
Sagor Ahmed <ashrafniloy512@gmail.com>
2024-11-06 19:24:34 +0600

    favicon added

--- src/Components/DashboardCardProduct.jsx
+++ src/Components/DashboardCardProduct.jsx
@@ -49,14 +49,15 @@ const DashboardCardProduct = () => {
   }
   return (
     <>
-      {cardItem.length > 0 ? <div className="flex justify-between items-center max-w-[1000px] mx-auto mt-10">
+      <div className="flex justify-between items-center max-w-[1000px] mx-auto mt-10">
         <div className="text-xl font-bold">Cart</div>
         <div className="flex items-center gap-4">
           <h1 className="text-xl font-bold">Total: {price}tk</h1>
           <button onClick={handleSortByPrice} className="text-purple-600 border border-purple-600 rounded-full px-3 py-2">Sort by price</button>
-          <button onClick={()=> setPurchase(true)} className="text-white bg-purple-600 px-3 py-2 rounded-full">Purchase</button>
+          <button disabled={cardItem.length <= 0} onClick={()=> setPurchase(true)} className="text-white bg-purple-600 px-3 py-2 rounded-full">Purchase</button>
         </div>
-      </div> : <p className="text-center pt-10 text-xl font-bold">Empty Card List</p>}
+      </div>
+      {cardItem.length <= 0 && <p className="text-center font-bold text-xl py-5">Emty Cart list</p>}
       {cardItem && cardItem.map((product, index) => (<CardProduct key={index} handleDeleteProduct={handleDeleteProduct} product={product} />))}
 
       {purchase && <Modal price={price} closePurchase={closePurchase}  />}
