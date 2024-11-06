4a7b4367e891f5232d6222049e6b9894d6d09978
Sagor Ahmed <ashrafniloy512@gmail.com>
2024-11-06 20:02:32 +0600

    readme.md file added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,11 +1,14 @@
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-[#9538E2] text-white">
        <div className="max-w-[1280px] mx-auto text-center px-24 pt-10 pb-64">
             <h1 className="text-5xl font-bold pb-6 leading-tight">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
             <p className="pb-8 text-sm">Explore the latest gadgets that will take your experience to the next level. From smart devices to <br />the coolest accessories, we have it all!</p>
-            <button className="bg-white px-5 py-2 text-lg rounded-full font-semibold text-[#9538E2]">Shop Now</button>
+        <button onClick={() => navigate("/dashboard")} className="bg-white px-5 py-2 text-lg rounded-full font-semibold text-[#9538E2]">Shop Now</button>
        </div>
     </div>
   )
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,9 @@
 
 const ErrorPage = () => {
   return (
-    <div>There is no content found.404</div>
+    <div>
+      <img className="w-screen h-screen" src="https://i.ibb.co.com/D7rbh0b/2153483.jpg" alt="this is is error image"/>
+    </div>
   )
 }
 
